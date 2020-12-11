809ea5a7588e837c5181038e8e480592f434a2ab
hanyoki <739319500@qq.com>
2020-12-11 17:37:53 +0800

    修改了组件2的内容

--- components/zuj2/zuj2.vue
+++ components/zuj2/zuj2.vue
@@ -1,15 +1,29 @@
 <template>
 	<view>
-		组件2
+		苹果MAC OS X怎么按照双系统
+		<image :src="item.img" v-for="item in swipers" :key="item.id"></image>
 	</view>
 </template>
 
 <script>
+	import {myRequest}from '@/utils/request.js'
 	export default {
 		data() {
 			return {
-				
+				swipers:[]
 			};
+		},
+		created(){
+			this.getSwipers();
+		},
+		methods:{
+			async getSwipers(){
+				const res = await myRequest({
+					method:'GET',
+					url:'/api/v1/fatiao/index/list?id=4&p=1&notchannel=27&order=1&limit=1&channel_id=4'
+				})
+				this.swipers = res.message
+			}
 		}
 	}
 </script>
