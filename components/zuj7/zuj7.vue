bf15bc6f42cef2c4705076f4568e3844c5bf1c82
zwq-009 <1097234514@qq.com>
2020-12-11 19:03:02 +0800

    组件7，兴趣框架，测试

--- components/zuj7/zuj7.vue
+++ components/zuj7/zuj7.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		组件7
+		<view class="top">
+			<text>如何画美人鱼</text>
+			<image src="" mode=""></image>
+			<text>兴趣爱好</text>
+			<image src="" mode=""></image><text>{{368}}</text>
+			<image src="" mode=""></image><text>{{2345}}</text>
+			<image src="" mode=""></image><text>{{分享}}</text>
+		</view>
 	</view>
 </template>
 
