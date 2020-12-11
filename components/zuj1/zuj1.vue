42267b83bf87493a80954e55871d11a4b591ee6b
Webwu <295870488@qq.com>
2020-12-11 19:52:58 +0800

    吴成真

--- components/zuj1/zuj1.vue
+++ components/zuj1/zuj1.vue
@@ -1,19 +1,40 @@
 <template>
-	<view>
-		组件1
+	<view class="jiaju">
+		<view class="fnx">
+			<text>丰年虾的孵化方法</text>
+			<image src="../../static/xia.png" mode="widthFix"></image>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {myRequestGet} from '@/utils/request.js'
 	export default {
 		data() {
 			return {
 				
 			};
+			methods:{
+				
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	 .jiaju {
+	    display: flex;
+	    width: 95%;
+	    margin: 0 auto;
+	    .fnx {
+	      flex: 1;
+	      text {
+	        display: block;
+	        margin: 12px 0px;
+	      }
+	     image{
+			 width: 100%;
+		 }
+	    }
+	  }
 </style>
