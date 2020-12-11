a1ac1236433c57dc1d4f28b9355673ed874d9714
zwq-009 <1097234514@qq.com>
2020-12-11 20:17:51 +0800

    组件7，首行

--- components/zuj7/zuj7.vue
+++ components/zuj7/zuj7.vue
@@ -1,12 +1,14 @@
 <template>
-	<view>
+	<view class="a">
 		<view class="top">
 			<text>如何画美人鱼</text>
-			<image src="" mode=""></image>
-			<text>兴趣爱好</text>
-			<image src="" mode=""></image><text>{{368}}</text>
-			<image src="" mode=""></image><text>{{2345}}</text>
-			<image src="" mode=""></image><text>{{分享}}</text>
+			<image src="https://vde.fatiao.pro/fatiao/xingqu/01/fmt/1.jpg" mode="aspectFit"></image>
+			<view class="bto">
+				<text class="btoa">兴趣爱好</text>
+				<image src="../../static/点赞.png" mode="aspectFit">379</image>
+				<image src="../../static/观看ico.png" mode="aspectFit">4742</image>
+				<image src="../../static/分享.png" mode="aspectFit">分享</image>
+			</view>
 		</view>
 	</view>
 </template>
@@ -15,12 +17,34 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.a{
+	.top{
+		text{
+			
+		}
+		.bto{
+			
+			height: 60rpx;
+			text-align: right;
+			.btoa{
+				float: left;
+			}
+			image{
+				width: 40rpx;
+				height: 40rpx;
+			}
+			text{
+				
+			}
+			
+		}
+	}
+}
 </style>
