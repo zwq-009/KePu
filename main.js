13105bbcac493a12dac326b153f0c8094fa5a84e
zwq-009 <1097234514@qq.com>
2020-12-14 16:00:42 +0800

    字体图片，组件7，

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
-
+import uniIcons from './components/uni-icons/uni-icons.vue'
+Vue.component('uniIcons',uniIcons)
 Vue.config.productionTip = false
 
 App.mpType = 'app'
