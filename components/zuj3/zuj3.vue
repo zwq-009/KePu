51eb983d8a4a080e076c2e81915a2cc9780912eb
zwq-009 <1097234514@qq.com>
2020-12-14 19:21:11 +0800

    组件7，兴趣界面。

---
+++ components/zuj3/zuj3.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="Box">
+		<!-- 请在这个大Box里自行写内容 -->
+		<view class="">
+			组件3
+		</view>
+		<view class="">
+			组件3
+		</view>
+		<view class="">
+			组件3
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.Box {
+	margin-top: 10px;
+	border-top-left-radius: 10px;
+	border-top-right-radius: 10px;
+	box-shadow: 0 -1px 3px 4px #f4f4f5;
+	
+	
+}
+</style>
---
+++ main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+import uniIcons from './components/uni-icons/uni-icons.vue'
+Vue.component('uniIcons',uniIcons)
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
