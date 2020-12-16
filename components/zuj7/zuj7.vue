a013a32bd6f6c040697885e558fe487ada2eafbb
zwq-009 <1097234514@qq.com>
2020-12-16 21:34:43 +0800

    调试组件7.视频分享与格式。

--- components/zuj7/zuj7.vue
+++ components/zuj7/zuj7.vue
@@ -3,33 +3,37 @@
 		<!-- 如何画美人鱼-->
 		<view class="a" v-for="item in swipers" :key="item.id">
 			<text class="top">{{item.title}}</text>
-			<image class="im" :src="item.info.video_img" mode="widthFix"></image>
+			<image @click="goVideo(item)" class="im" :src="item.info.video_img" mode="widthFix"></image>
 			<view class="atim">{{item.video_length}}</view>
 			<view class="bto">
 				<text class="btoa">{{item.channel.name}}</text>
-				<uni-icons type="hand-thumbsup" size="14" color="#a4b0be">fgsh</uni-icons>
+				<uni-icons type="hand-thumbsup" size="18" color="#a4b0be">fgsh</uni-icons>
 				<text>{{item.praise_num}}</text>
-				<uni-icons type="eye" size="14" color="#a4b0be"></uni-icons>
+				<uni-icons type="eye" size="18" color="#a4b0be"></uni-icons>
 				<text>{{item.play_num}}</text>
-				<uni-icons type="weixin" size="14" color="#00aa00" @click="shareOn"></uni-icons>
-				<text class="fen" @click="shareOn">分享</text>
+				<uni-icons type="redo" size="18" color="#00aa00" @click="shareOn"></uni-icons>
+				<text class="fen" @click.stop="shareOn">分享</text>
 			</view>
 		</view>
 
 		<!-- 列表 -->
 		<view class="ab" v-for="item in swipers1" :key="item.id">
-			<view class="aba">
-				<image :src="item.info.cover_img" mode="widthFix"></image>
+			<view class="aba" >
+				<view class="abim">
+					<image :src="item.info.cover_img" mode="widthFix" @click="goDetail(item)"></image>
+				</view>
 				<view class="abr">
 					<view class="abrt" @click="goDetail(item)">{{item.title}}</view>
 					<view class="abto">
 						<view class="abtot">{{item.channel.name}}</view>
-						<uni-icons type="hand-thumbsup" size="14" color="#a4b0be"></uni-icons>
-						<view>{{item.praise_num}}</view>
-						<uni-icons type="eye" size="14" color="#a4b0be"></uni-icons>
-						<view>7234</view>
-						<uni-icons type="weixin" size="15" color="#00aa00" @click="shareOn"></uni-icons>
-						<view class="fen" @click="shareOn">分享</view>
+						<view class=" abtor">
+							<uni-icons type="hand-thumbsup" size="16" color="#a4b0be"></uni-icons>
+							<view>{{item.praise_num}}</view>
+							<uni-icons type="eye" size="16" color="#a4b0be"></uni-icons>
+							<view>7234</view>
+							<uni-icons type="redo" size="16" color="#00aa00" @click="shareOn"></uni-icons>
+							<view class="fen" @click.stop="shareOn">分享</view>
+						</view>
 					</view>
 				</view>
 			</view>
@@ -46,29 +50,32 @@
 				<text>{{item.praise_num}}</text>
 				<uni-icons type="eye" size="18" color="#a4b0be"></uni-icons>
 				<text>{{item.play_num}}</text>
-				<uni-icons type="weixin" size="18" color="#00aa00" @click="shareOn"></uni-icons>
-				<text class="fen" @click="shareOn">分享</text>
+				<uni-icons type="redo" size="18" color="#00aa00" @click="shareOn"></uni-icons>
+				<text class="fen" @click.stop="shareOn">分享</text>
 			</view>
 		</view>
 		<!-- 画蛇列表 -->
 		<view class="ab" v-for="item in swipers3" :key="item.id">
 			<view class="aba">
-				<image :src="item.info.cover_img" mode="widthFix"></image>
+				<view class="abim" @click="goDetail(item)">
+					<image :src="item.info.cover_img" mode="widthFix"></image>
+				</view>
 				<view class="abr">
-					<view class="abrt">{{item.title}}</view>
+					<view class="abrt" @click="goDetail(item)">{{item.title}}</view>
 					<view class="abto">
-						<view class="abtot">兴趣爱好</view>
-						<uni-icons type="hand-thumbsup" size="14" color="#a4b0be"></uni-icons>
-						<view>{{item.praise_num}}</view>
-						<uni-icons type="eye" size="14" color="#a4b0be"></uni-icons>
-						<view>7234</view>
-						<uni-icons type="weixin" size="18" color="#00aa00" @click="shareOn"></uni-icons>
-						<view @click="shareOn">分享</view>
+						<view class="abtot">{{item.channel.name}}</view>
+						<view class=" abtor">
+							<uni-icons type="hand-thumbsup" size="16" color="#a4b0be"></uni-icons>
+							<view>{{item.praise_num}}</view>
+							<uni-icons type="eye" size="16" color="#a4b0be"></uni-icons>
+							<view>7234</view>
+							<uni-icons type="redo" size="16" color="#00aa00" @click="shareOn"></uni-icons>
+							<view class="fen" @click.stop="shareOn">分享</view>
+						</view>
 					</view>
 				</view>
 			</view>
 		</view>
-
 		<!-- 如何画花朵-->
 		<view class="a" v-for="item in swipers4" :key="item.id">
 			<text class="top">{{item.title}}</text>
@@ -80,24 +87,28 @@
 				<text>{{item.praise_num}}</text>
 				<uni-icons type="eye" size="18" color="#a4b0be"></uni-icons>
 				<text>{{item.play_num}}</text>
-				<uni-icons type="weixin" size="18" color="#00aa00" @click="shareOn"></uni-icons>
-				<text class="fen" @click="shareOn">分享</text>
+				<uni-icons type="redo" size="18" color="#00aa00" @click="shareOn"></uni-icons>
+				<text class="fen" @click.stop="shareOn">分享</text>
 			</view>
 		</view>
 		<!-- 玉米钓青鱼列表 -->
 		<view class="ab" v-for="item in swipers5" :key="item.id">
 			<view class="aba">
-				<image :src="item.info.cover_img" mode="widthFix"></image>
+				<view class="abim" @click="goDetail(item)">
+					<image :src="item.info.cover_img" mode="widthFix"></image>
+				</view>
 				<view class="abr">
-					<view class="abrt">{{item.title}}</view>
+					<view class="abrt" @click="goDetail(item)">{{item.title}}</view>
 					<view class="abto">
-						<view class="abtot">兴趣爱好</view>
-						<uni-icons type="hand-thumbsup" size="14" color="#a4b0be"></uni-icons>
-						<view>{{item.praise_num}}</view>
-						<uni-icons type="eye" size="14" color="#a4b0be"></uni-icons>
-						<view>7234</view>
-						<uni-icons type="weixin" size="18" color="#00aa00" @click="shareOn"></uni-icons>
-						<view @click="shareOn">分享</view>
+						<view class="abtot">{{item.channel.name}}</view>
+						<view class=" abtor">
+							<uni-icons type="hand-thumbsup" size="16" color="#a4b0be"></uni-icons>
+							<view>{{item.praise_num}}</view>
+							<uni-icons type="eye" size="16" color="#a4b0be"></uni-icons>
+							<view>7234</view>
+							<uni-icons type="redo" size="16" color="#00aa00" @click="shareOn"></uni-icons>
+							<view class="fen" @click.stop="shareOn">分享</view>
+						</view>
 					</view>
 				</view>
 			</view>
@@ -120,14 +131,6 @@
 	export default {
 		data() {
 			return {
-				sharedata: {
-					type: 1,
-					strShareUrl: "http://www.baidu.com",
-					strShareTitle: "分享标题",
-					strShareSummary: "分享总结",
-					strShareImageUrl: "http://www.xuelejia.com/xljapp/h5/static/aboutUsLogo.png"
-				},
-
 				swipers: [],
 				swipers1: [],
 				swipers2: [],
@@ -147,17 +150,22 @@
 			this.getSwipers4();
 			this.getSwipers5();
 		},
+		components: { //分享引入组件
+			uniPopup,
+			shareBtn
+		},
 		methods: {
-			shareOn() {
+			shareOn: function() {
 				this.$refs.sharepopup.open();
-			},
-			components: {
-				uniPopup,
-				shareBtn
 			},
 			goDetail(item) {
 				uni.navigateTo({
 					url: `/pages/detail07/detail07?id=${item.id}`
+				})
+			},
+			goVideo(item) {
+				uni.navigateTo({
+					url: `/pages/videos/videos7?id=${item.id}`
 				})
 			},
 			async getSwipers() {
@@ -230,7 +238,7 @@
 		}
 	};
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 	.Box {
 		margin-top: 10rpx;
 		border-top-left-radius: 10rpx;
@@ -246,7 +254,7 @@
 				margin-left: 20rpx;
 				font-size: 40rpx;
 				font-weight: 540;
-				background: linear-gradient(0deg, #2854f0 0%, #fff 20%, #fff 100%);
+				background: linear-gradient(0deg, #2854f0 0%, #fff 20%, #eef5f5 100%);
 			}
 
 			.im {
@@ -277,19 +285,18 @@
 					color: #000007;
 					margin-left: 20rpx;
 					float: left;
-					background: linear-gradient(0deg, #0055ff 0%, #fff 20%, #fff 100%);
+					background: linear-gradient(0deg, #0055ff 0%, #eef5f5 20%, #eef5f5 100%);
 				}
-
 				text {
 					opacity: 0.8;
-					font-size: 18rpx;
+					font-size: 27rpx;
 					color: #736c79;
 					font-weight: 650;
 					padding-right: 15rpx;
 				}
 
 				.fen {
-					font-size: 20rpx;
+					font-size: 27rpx;
 					color: #060607;
 
 				}
@@ -297,46 +304,58 @@
 		}
 
 		.ab {
-			padding: 20rpx 0;
+			padding: 2% 0.8%;
 			border-bottom: 1rpx solid #a7afa8;
 			background-color: #eef5f5;
 
 			.aba {
-				padding-left: 10rpx;
+				height: 151rpx;
+				margin-left: 0.7%;
 
-				image {
-					width: 260rpx;
-					margin-right: 15rpx;
-					border-radius: 20rpx;
-					float: left;
+				.abim {
+					image {
+						// padding: 1% 1%;
+						width: 33%;
+						border-radius: 20rpx;
+						float: left;
+					}
 				}
 
 				.abr {
+					margin-left: 0.7%;
+					float: left;
+					width: 65%;
+
 					.abrt {
-						font-size: 35rpx;
-						padding: 0 0 50rpx;
-						font-weight: 400;
+						font-size: 38rpx;
+						font-weight: 600;
+						color: #5b6356;
+						margin: 0 0 10%;
 					}
 
 					.abto {
-						text-align: left;
-						padding: 0 0 5rpx;
+						text-align: right;
+						margin: 0 2% 0 0;
 
 						.abtot {
 							font-size: 19rpx;
 							font-weight: 600;
-							display: inline-block;
-							margin-right: 11%;
+							float: left;
 							background: linear-gradient(0deg, #0055ff 0%, #fff 40%, #fff 100%);
 						}
 
-						view {
-							font-weight: 550;
-							opacity: 0.7;
-							font-size: 17rpx;
-							display: inline-block;
-							padding-right: 0.8%;
+						.abtor {
+							font-size: 19rpx;
+							margin: 0 0 1%;
+							view {
+								font-weight: 550;
+								opacity: 0.7;
+								font-size: 26rpx;
+								display: inline-block;
+								padding-right: 2.5%;
+							}
 						}
+
 					}
 				}
 			}
--- pages/detail07/detail07.vue
+++ pages/detail07/detail07.vue
@@ -2,55 +2,85 @@
 	<view>
 		<view class="a">
 			<view class="aa">{{xqsj.query}}</view>
+			<image class="author_img" :src="xqsj.channel.channel_img" mode="widthFix"></image>
 			<view class="ab">{{xqsj.author.name}}</view>
-			<view class="ac">{{xqsj.add_time}}</view>
+			<view class="ac">2019-07-28</view>
 			<view class="">
 				<text>{{xqsj.info.name}}</text>
 			</view>
 			<view class="content">
 				<rich-text :nodes="xqsj.info.content.replace(/\<img/gi, '<img style=max-width:96%;height:auto;padding:7px')"></rich-text>
 			</view>
+			<view class="bto">
+				<uni-icons type="eye" size="19" color="#a4b0be"></uni-icons>
+				<text>{{xqsj.play_num}}</text>
+				<uni-icons @click="doSupport" type="hand-thumbsup" size="19" color="#a4b0be">fgsh</uni-icons>
+				<text>{{xqsj.praise_num}}</text>
 
-			<view class="">
-				<text>猜你喜欢</text>
+				<view class="btor">
+					<uni-icons type="redo" size="19" color="#00aa00" @click="shareOn"></uni-icons>
+					<text @click.stop="shareOn">分享</text>
+				</view>
 			</view>
-
 		</view>
+
+		<uni-popup ref="sharepopup" type="bottom">
+			<share-btn :sharedataTemp="sharedata"></share-btn>
+		</uni-popup>
 	</view>
+	</view>
+
 </template>
 
 <script>
 	import {
 		myRequestGet
 	} from '@/utils/request.js';
+	// import {
+	// 	formatRichText
+	// } from '@/utils/format.js'
+	// import parse from '@/utils/htmlparser.js';
+
 	export default {
 		data() {
 			return {
 				id: "",
-				xqsj: {}
+				xqsj: {},
 			};
 		},
 		onLoad(options) {
 			this.id = options.id;
 			this.getNewsDetail()
-			console.log(this.id)
+			this.getDetailContent()
 		},
-
 		methods: {
 			async getNewsDetail() {
 				const res = await myRequestGet('/api/v1/fatiao/article/detail?id=' + this.id)
-				console.log(res)
-				//#ifdef MP-ALIPAY
-				this.htmlNodes = parse(this.newsDetail.content)
-				//#endif
 				this.xqsj = res
-			}
+			},
+			async getDetailContent() {
+				const res = await myRequestGet('/api/v1/fatiao/article/detail?id=' + this.id)
+				this.content = formatRichText(res.info.content)
+				this.htmlNodes = parse(this.content)
+			},
+			shareOn: function() {
+				this.$refs.sharepopup.open();
+			},
 		}
 	};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.a {
+		padding: 0 4%;
+
+		.author_img {
+			display: block;
+			float: left;
+			border-radius: 50%;
+			width: 7%;
+		}
+
 		.aa {
 			padding: 4% 2%;
 			font-size: 45rpx;
@@ -59,26 +89,44 @@
 		}
 
 		.ab {
-			font-size: 15rpx;
+			font-size: 28rpx;
+			font-weight: 550;
 			padding: 1% 2% 0;
 			float: left;
 		}
 
 		.ac {
-			font-size: 15rpx;
+			font-size: 28rpx;
+			font-weight: 550;
 			padding: 1% 2% 0;
+			opacity: 0.5;
 		}
 
 		.content {
-			padding: 15rpx;
-			font-size: 32rpx;
+			padding: 5% 1% 5%;
+			font-weight: 500;
+			font-size: 35rpx;
+			line-height: 60rpx;
 			color: gray;
 			text-indent: 50rpx;
+			border-bottom: 1rpx solid #b7bfe1;
 
 			img {
 				width: 90%;
 				height: 50%;
 
+			}
+		}
+
+		.bto {
+			padding: 5% 3%;
+			text-align: left;
+			line-height: 46rpx;
+			letter-spacing: 5rpx;
+
+			.btor {
+				display: inline-block;
+				float: right;
 			}
 		}
 	}
